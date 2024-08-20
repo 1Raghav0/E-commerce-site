a170caf7965d716235820d1d3fdc41b140513db6
1Raghav0 <raghav514422@gmail.com>
2024-08-20 13:38:32 +0530

    raghvendra

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ MenuItems.style.maxHeight == "0px"
 
 function menutoggle(){
     if(MenuItems.style.maxHeight == "0px"){
-        MenuItems.style.maxHeight = "200px"
+        MenuItems.style.maxHeight = "250px"
     }
     else 
     {
@@ -33,20 +33,20 @@ function menutoggle(){
 
         // js for account page
 
-     var LoginForm = document.getElementById("LoginForm");   
-     var RegForm = document.getElementById("RegForm");   
-     var Indicator = document.getElementById("Indicator");   
-
-     function register(){
-
-        RegForm.style.transform = "translateX(0px)";
-        LoginForm.style.transform = "translateX(0px)";
-        Indicator.style.transform = "translateX(100px)";
-     }
-
-     function login(){
-
-        RegForm.style.transform = "translateX(300px)";
-        LoginForm.style.transform = "translateX(300px)";
-        Indicator.style.transform = "translateX(0px)";
-     }
+        var LoginForm = document.getElementById("LoginForm");   
+        var RegForm = document.getElementById("RegForm");   
+        var Indicator = document.getElementById("Indicator");   
+   
+        function register(){
+   
+           RegForm.style.transform = "translateX(0px)";
+           LoginForm.style.transform = "translateX(0px)";
+           Indicator.style.transform = "translateX(100px)";
+        }
+   
+        function login(){
+   
+           RegForm.style.transform = "translateX(300px)";
+           LoginForm.style.transform = "translateX(300px)";
+           Indicator.style.transform = "translateX(0px)";
+        }
